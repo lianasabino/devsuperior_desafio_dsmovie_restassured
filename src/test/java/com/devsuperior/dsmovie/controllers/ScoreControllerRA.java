d47ec7e7483caa1dd28c0e2ef7006c537a6a73bf
lianasabino <lianasabino@hotmail.com>
2025-04-29 20:04:33 -0300

    Adding tests at ScoreControllerRA

--- src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,18 +1,101 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.devsuperior.dsmovie.tests.TokenUtil;
+
+import io.restassured.http.ContentType;
 
 public class ScoreControllerRA {
 	
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken;
+	
+	private Long existingScoreId, nonExistingScoreId;
+	
+	private Map<String, Object> putScoreInstance;
+	
+	@BeforeEach
+	public void setup() throws JSONException {
+		baseURI = "http://localhost:8080";
+		
+		clientUsername = "alex@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "maria@gmail.com";
+		adminPassword = "123456";
+		
+		existingScoreId = 1L;
+		nonExistingScoreId = 100L;
+		
+		putScoreInstance = new HashMap<>();
+		putScoreInstance.put("movieId", existingScoreId);
+		putScoreInstance.put("score", 4);
+		
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+		
+	}
+	
 	@Test
 	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
+		
+		putScoreInstance.put("movieId", nonExistingScoreId);
+		JSONObject newScore = new JSONObject(putScoreInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.body(newScore)
+		.when()
+			.put("/scores")
+		.then()
+			.statusCode(404);
 	}
 	
 	@Test
 	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+		
+		putScoreInstance.put("movieId", null);
+		JSONObject newScore = new JSONObject(putScoreInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.body(newScore)
+		.when()
+			.put("/scores")
+		.then()
+			.statusCode(422);
 	}
 	
 	@Test
 	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {		
+		
+		putScoreInstance.put("score", -50);
+		JSONObject newScore = new JSONObject(putScoreInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.body(newScore)
+		.when()
+			.put("/scores")
+		.then()
+			.statusCode(422);
 	}
 }
